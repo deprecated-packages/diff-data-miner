cc7e86833584b496acb7244e361ed8655292094f
TomasVotruba <tomas.vot@gmail.com>
2020-09-16 16:49:59 +0200

    indent

---
+++ config/config.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\SmartFileSystem\SmartFileSystem;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+
+    $services->defaults()
+        ->public()
+        ->autowire()
+        ->autoconfigure();
+
+    $services->load('Migrify\DiffDataMiner\\', __DIR__ . '/../src')
+        ->exclude([__DIR__ . '/../src/HttpKernel']);
+
+    $services->set(SmartFileSystem::class);
+};
